232330c5ba23921ff891418c6562e07886caf5b0
PRATHAM GUPTA <PRATHAM17GUPTA@GMAIL.COM>
2024-04-21 13:51:45 +0530

    Configured routes and authentication

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1 className='h1-bold'>Answer Hub</h1>
-      <h1 className='h2-bold'>Answer Hub</h1>
-      <h1 className='h3-bold'>Answer Hub</h1>
-    </>
-  );
-}
