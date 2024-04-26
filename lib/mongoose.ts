28c229b50c9a31c45bb5119f7c3b3730c11ff875
PRATHAM GUPTA <PRATHAM17GUPTA@GMAIL.COM>
2024-04-27 01:35:38 +0530

    updated db name

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -15,7 +15,7 @@ export const connectToDatabase = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
-      dbName: 'dev-overflow',
+      dbName: 'answer-hub',
     });
 
     isConnected = true;
